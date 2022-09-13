d3f415891269a6294f69c222fbdb7a7df0e9db60
Vladimir Banduristov <vaban@norvikbank.ru>
2022-09-13 10:16:23 +0300

    fix: update

--- src/components/Base/SvButton/SvButton.vue
+++ src/components/Base/SvButton/SvButton.vue
@@ -16,8 +16,7 @@
   </component>
 </template>
 
-<script setup lang="ts">
-import { computed } from "vue";
+<script lang="ts">
 import {IEvent} from "@/components/types";
 
 interface ISvButtonProps {
@@ -43,6 +42,10 @@ interface ISvButtonProps {
 interface ISvButtonEmits {
   (event: "click", value: object): void;
 }
+</script>
+
+<script setup lang="ts">
+import { computed } from "vue";
 
 const props = withDefaults(defineProps<ISvButtonProps>(), {
   iconRight: false,
